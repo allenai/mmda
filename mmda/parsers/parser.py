9701e6c9cbd57cb8d6329e145d6f91e8aa70ca7a
Shannon Shen <22512825+lolipopshock@users.noreply.github.com>
2021-07-13 00:56:11 -0400

    Add the PDF Image loader

--- mmda/parsers/parser.py
+++ mmda/parsers/parser.py
@@ -6,13 +6,23 @@ Dataclass for creating token streams from a document
 
 """
 
-from typing import Optional, Union
+from typing import Optional, Union, List
+
+from pdf2image import convert_from_path
 
 from mmda.types.document import Document
 
+
 class Parser:
-    def parse(self, infile: str, outdir: Optional[str] = None, outfname: Optional[str] = None) -> Union[str, Document]:
+    def parse(
+        self, infile: str, outdir: Optional[str] = None, outfname: Optional[str] = None
+    ) -> Union[str, Document]:
         raise NotImplementedError
 
     def load(self, infile: str) -> Document:
         raise NotImplementedError
+
+    def load_images(self, infile: str) -> List["PIL.Image"]:
+
+        images = convert_from_path(infile, dpi=72) #Though 72 is not the default dpi for pdf2image, it's commonly used by other PDF parsing systems 
+        return images
