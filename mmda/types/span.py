b4c9aa6b0e4a1df76ba8679014c22ede929f430e
kyleclo <kyleclo@uw.edu>
2021-08-03 13:29:04 -0700

    spans now contains boxes

--- mmda/types/span.py
+++ mmda/types/span.py
@@ -8,15 +8,24 @@ from typing import List, Optional, Dict, Tuple, Type
 from abc import abstractmethod
 from dataclasses import dataclass, field
 
+from mmda.types.box import Box
+
 
 @dataclass
 class Span:
     start: int
     end: int
     page: int
+    box: Optional[Box] = None
+
+    def __post_init__(self):
+        assert self.box.page == self.page, f"Box.page {self.box.page} != Page {self.page}"
 
     def to_json(self) -> Dict:
-        return dict(start=self.start, end=self.end, page=self.page)
+        if self.box:
+            return dict(start=self.start, end=self.end, page=self.page)
+        else:
+            return dict(start=self.start, end=self.end, page=self.page, box=self.box)
 
 
 @dataclass
