f7e48fb9f213a3ad67ff7fa4e772fa4145bd96f3
Shannon Shen <22512825+lolipopshock@users.noreply.github.com>
2021-08-10 16:00:09 -0400

    remove model config

--- mmda/models/base_predictor_config.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-from typing import Union, List, Dict, Any
-
-# TODO[Shannon] Not exactly sure about the model configurations till
-# we add specific models.
-@dataclass
-class BaseDocumentPredictorConfig:
-    model_name: str = None
-    model_class: Any = None
