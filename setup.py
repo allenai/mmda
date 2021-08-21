48843dd4e5e799faa9280f1866eea675f24c66a1
kyleclo <kyleclo@uw.edu>
2021-08-20 23:28:51 -0700

    init setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+import setuptools
+
+setuptools.setup(
+    name='mmda',
+    version='0.0.1',
+    packages=setuptools.find_packages(),
+    install_requires=[
+    ],
+    tests_require=[
+    ],
+    zip_safe=False,
+    test_suite='py.test',
+    entry_points='',
+)
