f06c396af671d23766fc351d8648505203296f42
Yoganand Chandrasekhar <yogic@allenai.org>
2021-08-23 16:33:07 -0700

    update reqs and setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,10 @@
-#!/usr/bin/python
 import setuptools
 
 setuptools.setup(
     name='mmda',
-    version='0.0.1',
-    packages=setuptools.find_packages(),
-    install_requires=[
-    ],
-    tests_require=[
-    ],
-    zip_safe=False,
-    test_suite='py.test',
-    entry_points='',
+    version='0.0.2',
+    python_requires=">= 3.8",
+    packages=setuptools.find_packages(include=["mmda.*"]),
+    install_requires=['intervaltree', 'pdf2image'],
+    extras_require={"dev": ["pytest"]}
 )
