ec5bca515e18905089848a607cae0b12db910ea8
Yoganand Chandrasekhar <yogic@allenai.org>
2021-08-25 09:14:23 -0700

    format

--- services/parsers/symbolscraper/setup.py
+++ services/parsers/symbolscraper/setup.py
@@ -12,9 +12,7 @@ s2agemaker_requirements = [
 ]
 
 # Add your python dependencies
-model_requirements = [
-    "mmda@git+https://github.com/allenai/mmda.git@0.0.2"
-]
+model_requirements = ["mmda@git+https://github.com/allenai/mmda.git@0.0.2"]
 
 dev_requirements = ["pytest", "mypy", "black", "requests", "types-requests"]
 
