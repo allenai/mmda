e2e21020a60a11ab61bdd030c2a4911bd1e7a547
kyleclo <kyleclo@uw.edu>
2021-10-13 12:12:06 -0700

    migrate image rasterize functions into a new class

--- mmda/parsers/parser.py
+++ mmda/parsers/parser.py
@@ -15,7 +15,10 @@ from mmda.types.image import load_pdf_images_from_path
 
 class Parser(Protocol):
     @abstractmethod
-    def parse(self, input_pdf_path: str, **kwargs) -> Document:
+    def parse(self,
+              input_pdf_path: str,
+              output_json_path: Optional[str] = None,
+              **kwargs) -> Document:
         """Given an input PDF return a Document with at least symbols
 
         Args:
@@ -24,48 +27,3 @@ class Parser(Protocol):
         Returns:
             Document: Depending on parser support at least symbols in the PDF
         """
-
-
-class BaseParser:
-
-    DEFAULT_PDF_RENDERING_DPI = 72
-
-    def __init__(self, dpi: int = None) -> None:
-        """
-        dpi (int, optional):
-            Used for specify the resolution (or `DPI, dots per inch
-            <https://en.wikipedia.org/wiki/Dots_per_inch>`_) when loading images of
-            the pdf. Higher DPI values mean clearer images (also larger file sizes).
-
-            Defaults to `self.DEFAULT_PDF_RENDERING_DPI=72`.
-        """
-
-    @property
-    def dpi(self) -> int:
-        return self._dpi
-
-    @dpi.setter
-    def dpi(self, dpi: int):
-        self._dpi = dpi if dpi is not None else self.DEFAULT_PDF_RENDERING_DPI
-
-    @abstractmethod
-    def parse(
-        self,
-        input_pdf_path: str,
-        output_json_path: Optional[str] = None,
-        tempdir: Optional[str] = None,
-        load_images: bool = False,
-    ) -> Union[str, Document]:
-        """This is the main entrance point for using the PDF parsers. For a
-        given PDF file, this method will return a Document object.
-        """
-
-    # TODO[kylel] - serialization?
-    def load_images(self, input_pdf_path: str, dpi=None) -> List["PIL.Image"]:
-        if dpi is None:
-            dpi = self.dpi
-        else:
-            self.dpi = dpi
-        images = load_pdf_images_from_path(input_pdf_path, dpi=dpi)
-        # Though 72 is not the default dpi for pdf2image, it's commonly used by other PDF parsing systems
-        return images
---
+++ mmda/rasterizers/rasterizer.py
@@ -0,0 +1,24 @@
+
+from abc import abstractmethod
+from typing import List, Optional, Protocol, Union
+
+from mmda.types.document import Document
+from mmda.types.image import load_pdf_images_from_path
+
+
+class Rasterizer(Protocol):
+    def rasterize(self, input_pdf_path: str, dpi: int, **kwargs) -> List["PIL.Image"]:
+        """Given an input PDF return a List[Image]
+
+        Args:
+            input_pdf_path (str): Path to the input PDF to process
+            dpi (int): Used for specify the resolution (or `DPI, dots per inch
+                       <https://en.wikipedia.org/wiki/Dots_per_inch>`_) when loading images of
+                       the pdf. Higher DPI values mean clearer images (also larger file sizes).
+
+        Returns:
+            List[Image]
+        """
+        images = load_pdf_images_from_path(pdf_path=input_pdf_path, dpi=dpi)
+        return images
+
