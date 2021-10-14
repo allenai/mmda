0804b02403ff6b112f3e9e5ff568c08be493f3cc
Yoganand Chandrasekhar <yogic@allenai.org>
2021-10-14 09:52:00 -0700

    remove services

--- services/parsers/symbolscraper/model/instance.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel, Field
-
-
-class Instance(BaseModel):
-    """Represents one object for which inference can be performed."""
-
-    pdf: str = Field(description="Base64 encoded bytes of PDF")
