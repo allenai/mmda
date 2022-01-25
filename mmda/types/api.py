1c0836f7d7a878843bbb6160faf5023f51fe6e5d
Yoganand Chandrasekhar <yogic@allenai.org>
2022-01-24 17:00:11 -0800

    add api types

---
+++ mmda/types/api.py
@@ -0,0 +1,88 @@
+from __future__ import annotations
+
+from typing import List, Optional
+from pydantic import BaseModel, validator
+
+import mmda.types.annotation as mmda_ann
+
+
+def _validate_box(box: List[float]) -> None:
+    assert len(box) == 5, "Box must have format [x, y, w, h, page]"
+    assert int(box[4]) == box[4], "Page must be an integer value"
+
+
+class Span(BaseModel):
+    start: int
+    end: int
+    box: Optional[List[float]]
+
+    @classmethod
+    def from_mmda(cls, span: mmda_ann.Span) -> Span:
+        ret = cls(start=span.start, end=span.end)
+        if span.box is not None:
+            ret.box = span.box.to_json()
+        return ret
+
+    def to_mmda(self) -> mmda_ann.Span:
+        ret = mmda_ann.Span(start=self.start, end=self.end)
+        if self.box is not None:
+            ret.box = mmda_ann.Box.from_json(self.box)
+        return ret
+
+    @validator('box')
+    def validate_box(cls, maybe_box):
+        if maybe_box is not None:
+            _validate_box(maybe_box)
+        return maybe_box
+
+
+class BoxGroup(BaseModel):
+    boxes: List[List[float]]
+    id: Optional[int]
+    type: Optional[str]
+
+    @classmethod
+    def from_mmda(cls, box_group: mmda_ann.BoxGroup) -> BoxGroup:
+        boxes = [box.to_json() for box in box_group.boxes]
+        return cls(boxes=boxes, id=box_group.id, type=box_group.id)
+
+    def to_mmda(self) -> mmda_ann.BoxGroup:
+        boxes = [mmda_ann.Box.from_json(box) for box in self.boxes]
+        return mmda_ann.BoxGroup(boxes=boxes, id=self.id, type=self.type)
+
+    @validator('boxes')
+    def validate_boxes(cls, boxes):
+        for box in boxes:
+            _validate_box(box)
+        return boxes
+
+
+class SpanGroup(BaseModel):
+    spans: List[Span]
+    box_group: Optional[BoxGroup]
+    id: Optional[int]
+    type: Optional[str]
+    text: Optional[str]
+
+    @classmethod
+    def from_mmda(cls, span_group: mmda_ann.SpanGroup) -> SpanGroup:
+        ret = cls(
+            spans=[Span.from_mmda(sp) for sp in span_group.spans],
+            id=span_group.id,
+            type=span_group.type,
+            text=span_group.text
+        )
+        if span_group.box_group is not None:
+            ret.box_group = BoxGroup.from_mmda(span_group.box_group)
+        return ret
+
+    def to_mmda(self) -> mmda_ann.SpanGroup:
+        ret = mmda_ann.SpanGroup(
+            spans=[sp.to_mmda() for sp in self.spans],
+            id=self.id,
+            type=self.type,
+            text=self.text
+        )
+        if self.box_group is not None:
+            ret.box_group = self.box_group.to_mmda()
+        return ret
--- pipeline/run.py
+++ pipeline/run.py
@@ -16,27 +16,43 @@ pdf_file_prefix = '.'.join(os.path.abspath(sys.argv[1]).split(".")[:-1])
 pdf_file = f"{pdf_file_prefix}.pdf"
 document_file = f"{pdf_file_prefix}-document.json"
 elements_file = f"{pdf_file_prefix}-elements.json"
+parser_file = f"{pdf_file_prefix}-parser.json"
+blocks_file = f"{pdf_file_prefix}-blocks.json"
+spans_file = f"{pdf_file_prefix}-spans.json"
+words_file = f"{pdf_file_prefix}-words.json"
 
 print(f"reading pdf from from {pdf_file}")
 print(f"writing document to   {document_file}")
 print(f"writing elements to   {elements_file}")
+print(f"writing parser   to   {parser_file}")
+print(f"writing blocks   to   {blocks_file}")
+print(f"writing spans    to   {spans_file}")
+print(f"writing words    to   {words_file}")
 
 with open(f"{pdf_file_prefix}.pdf", "rb") as f:
     pdf_bytes = f.read()
 
 doc = Document.from_json(requests.post("http://localhost:8081", data=pdf_bytes).json())
+with open(parser_file, "w") as f:
+    json.dump(doc.to_json(), f, indent=2)
 
 resp = requests.post("http://localhost:8082", json=doc.to_json(with_images=True))
 box_groups = [BoxGroup.from_json(p) for p in resp.json()]
 doc.annotate(blocks=box_groups)
+with open(blocks_file, "w") as f:
+    json.dump([bg.to_json() for bg in box_groups], f, indent=2)
 
 resp = requests.post("http://localhost:8083", json=doc.to_json(with_images=True))
 span_groups = [SpanGroup.from_json(p) for p in resp.json()]
 doc.annotate(vila_spans=span_groups)
+with open(spans_file, "w") as f:
+    json.dump([sg.to_json() for sg in span_groups], f, indent=2)
 
 dictionary_word_predictor = DictionaryWordPredictor("/dev/null")
 words = dictionary_word_predictor.predict(doc)
 doc.annotate(words=words)
+with open(words_file, "w") as f:
+    json.dump([sg.to_json() for sg in words], f, indent=2)
 
 with open(document_file, "w") as f:
     json.dump(doc.to_json(), f)  # add with_images=True if you want.
