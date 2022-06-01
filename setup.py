c2a25165f60187afd8fa77ed595651ed146f77f4
David Graham <david.l.graham1@gmail.com>
2022-06-01 15:35:35 -0700

    Migrating layout parser interface to timo (#81)
    * Migrating layout parser interface to timo
    
    Co-authored-by: David Graham <davidg@ip-192-168-255-252.us-west-2.compute.internal>
    Co-authored-by: David Graham <davidg@David-Grahams-MacBook-Pro.local>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="mmda",
-    version="0.0.7",
+    version="0.0.8",
     python_requires=">= 3.7",
     packages=setuptools.find_packages(include=["mmda*", "ai2_internal*"]),
     install_requires=[
