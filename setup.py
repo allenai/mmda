1ead6ee9635fcb4e707cc2c98ea2d3046b27ab31
Chris Wilhelm <chris@allenai.org>
2022-06-29 12:22:51 -0700

    bump version and fix vila test fixture inclusion

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="mmda",
-    version="0.0.8",
+    version="0.0.9",
     python_requires=">= 3.7",
     packages=setuptools.find_packages(include=["mmda*", "ai2_internal*"]),
     install_requires=[
