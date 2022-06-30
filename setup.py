fd00b477583bec5e9556e66a183885c1e3b43bac
Chris Wilhelm <chris.wilhelm@gmail.com>
2022-06-30 12:25:34 -0700

    Pins vila to 0.3.0. (#86)
    Unbounded upper version was pulling in breaking
    changes transitively in builds.
    
    Co-authored-by: Chris Wilhelm <chris@allenai.org>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="mmda",
-    version="0.0.10",
+    version="0.0.11",
     python_requires=">= 3.7",
     packages=setuptools.find_packages(include=["mmda*", "ai2_internal*"]),
     install_requires=[
@@ -19,7 +19,7 @@ setuptools.setup(
         "api": ["Flask", "gevent"],
         "pipeline": ["requests"],
         "lp_predictors": ["layoutparser", "torch", "torchvision", "effdet"],
-        "vila_predictors": ["vila >= 0.3.0", "transformers"],
+        "vila_predictors": ["vila==0.3.0", "transformers"],
         "bibentry_predictor": ["transformers", "unidecode", "torch"],
     },
     include_package_data=True,
