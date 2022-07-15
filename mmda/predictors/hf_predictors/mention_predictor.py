b601cc72f579cd2983e2e685f5f7b4eb6263ccad
Chris Wilhelm <chris@allenai.org>
2022-07-15 10:25:44 -0700

    switch to optimum for runtime

--- mmda/predictors/hf_predictors/mention_predictor.py
+++ mmda/predictors/hf_predictors/mention_predictor.py
@@ -1,7 +1,6 @@
 import os
 
-import logging as logger
-from onnxruntime import InferenceSession
+from optimum.onnxruntime import ORTModelForTokenClassification
 import torch
 from transformers import AutoModelForTokenClassification, AutoTokenizer, PretrainedConfig
 from typing import List
@@ -13,15 +12,13 @@ from mmda.types.span import Span
 
 class MentionPredictor:
     def __init__(self, artifacts_dir: str):
-        self.model_config  = PretrainedConfig.from_pretrained(artifacts_dir)
         self.tokenizer = AutoTokenizer.from_pretrained(artifacts_dir)
 
         onnx_model_path = os.path.join(artifacts_dir, "model.onnx")
 
         if os.path.exists(onnx_model_path):
-            self.onnx_session = InferenceSession(os.path.join(artifacts_dir, "model.onnx"))
+            self.model = ORTModelForTokenClassification.from_pretrained(artifacts_dir, file_name="model.onnx")
         else:
-            self.onnx_session = None
             self.model = AutoModelForTokenClassification.from_pretrained(artifacts_dir)
 
     def predict(self, doc: Document) -> List[SpanGroup]:
@@ -31,33 +28,17 @@ class MentionPredictor:
             words: List[str] = ["".join(token.symbols) for token in page.tokens]
             word_spans: List[List[Span]] = [token.spans for token in page.tokens]
 
-            if self.onnx_session:
-                inputs = self.tokenizer(
-                    [words],
-                    is_split_into_words=True,
-                    max_length=512,
-                    truncation=True,
-                    padding='max_length',
-                    return_overflowing_tokens=True,
-                    return_tensors="np"
-                )
-                logits = torch.FloatTensor(self.onnx_session.run(
-                    output_names=["logits"],
-                    input_feed=dict(inputs)
-                )).squeeze(dim=0)
-            else:
-                inputs = self.tokenizer(
-                    [words],
-                    is_split_into_words=True,
-                    max_length=512,
-                    truncation=True,
-                    padding='max_length',
-                    return_overflowing_tokens=True,
-                    return_tensors="pt"
-                )
-                logits = self.model(**inputs).logits
-
+            inputs = self.tokenizer(
+                [words],
+                is_split_into_words=True,
+                max_length=512,
+                truncation=True,
+                padding='max_length',
+                return_overflowing_tokens=True,
+                return_tensors="pt"
+            )
             del inputs["overflow_to_sample_mapping"]
+            logits = self.model(**inputs).logits
 
             prediction_label_ids = torch.argmax(logits, dim=-1)
 
@@ -86,8 +67,8 @@ class MentionPredictor:
                 for word_id, label_ids in zip(word_ids, word_label_ids):
                     spans = word_spans[word_id]
 
-                    has_begin = any(self.model_config.id2label[int(lbl)] == "B-MENTION" for lbl in label_ids)
-                    has_inside = any(self.model_config.id2label[int(lbl)] == "I-MENTION" for lbl in label_ids)
+                    has_begin = any(self.model.config.id2label[int(lbl)] == "B-MENTION" for lbl in label_ids)
+                    has_inside = any(self.model.config.id2label[int(lbl)] == "I-MENTION" for lbl in label_ids)
 
                     if has_inside:
                         acc.extend(spans)
