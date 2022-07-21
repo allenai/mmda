76f2340eeec0de152c26bf4ee16342eda8214d16
Chris Wilhelm <chris@allenai.org>
2022-07-21 09:57:55 -0700

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="mmda",
-    version="0.0.15",
+    version="0.0.16",
     python_requires=">= 3.7",
     packages=setuptools.find_packages(include=["mmda*", "ai2_internal*"]),
     install_requires=[
