a14944650bfff5414f6cbc198e14f54e60f70694
Chris Wilhelm <chris@allenai.org>
2022-07-22 14:41:44 -0700

    Removes deepcopy from annotation method.
    Speeds up VILA runtime by ~10%,
    doesn't affect output.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(
     name="mmda",
     description="mmda",
-    version="0.0.17-dev0",
+    version="0.0.17",
     url="https://www.github.com/allenai/mmda",
     python_requires=">= 3.7",
     packages=setuptools.find_packages(include=["mmda*", "ai2_internal*"]),
