af433946b5a3675c8a43e5b6d43dbd8f8078bbe7
Egor Klevak <egork@allenai.org>
2022-09-08 15:00:32 -0700

    Adding merge spans with x, y overlap

--- mmda/utils/tools.py
+++ mmda/utils/tools.py
@@ -1,6 +1,8 @@
 from typing import List, Union, Dict, Any, Tuple, Optional
+from collections import defaultdict
 
 from mmda.types.span import Span
+from mmda.types.box import Box
 
 
 def merge_neighbor_spans(spans: List[Span], distance: int) -> List[Span]:
@@ -67,3 +69,70 @@ def allocate_overlapping_tokens_for_box(
         else:
             remaining_tokens.append(token)
     return allocated_tokens, remaining_tokens
+
+
+class MergeSpans:
+    """
+    Given w=width and h=height overlap neighboring spans which are w, h or less apart
+    Inspired by https://leetcode.com/problems/merge-intervals/
+    """
+    def __init__(self, list_of_spans: List["Span"], w: float, h: float) -> None:
+        self.list_of_spans = list_of_spans
+        self.w = w
+        self.h = h
+        self.graph = defaultdict(list)
+
+    def build_graph(self):
+        """
+        Build graph, each node is represented by (start, end) of tuple, with the list of spans with overlapping
+        boxes given, w, h
+        """
+        for i, span_i in enumerate(self.list_of_spans):
+            for j in range(i + 1, len(self.list_of_spans)):
+                if span_i.box.is_overlap(self.list_of_spans[j].box, self.w, self.h):
+                    self.graph[span_i.start, span_i.end].append(self.list_of_spans[j])
+                    self.graph[self.list_of_spans[j].start, self.list_of_spans[j].end].append(span_i)
+
+    # gets the connected components of the boxes overlap graph.
+    def get_components(self):
+        """
+        Groups connected graph nodes into dictionary list
+        """
+        visited = set()
+        comp_number = 0
+        nodes_in_comp = defaultdict(list)
+
+        def mark_component_dfs(start):
+            stack = [start]
+            while stack:
+                span = stack.pop()
+                node = span.start, span.end
+                if node not in visited:
+                    visited.add(node)
+                    nodes_in_comp[comp_number].append(span)
+                    stack.extend(self.graph[node])
+
+        # mark all nodes in the same connected component with the same integer.
+        for span in self.list_of_spans:
+            center = span.start, span.end
+            if center not in visited:
+                mark_component_dfs(span)
+                comp_number += 1
+
+        return nodes_in_comp, comp_number
+
+    def merge(self):
+        """
+        For each of the lists of the connected nodes, merge boxes and find, min, max of the index
+        """
+        self.build_graph()
+        nodes_in_comp, number_of_comps = self.get_components()
+
+        # all intervals in each connected component must be merged.
+        merged_spans = []
+        for comp in range(number_of_comps):
+            if nodes_in_comp[comp]:
+                merged_box = Box.small_boxes_to_big_box([span.box for span in nodes_in_comp[comp]])
+                merged_spans.append(Span(start=min([span.start for span in nodes_in_comp[comp]]),
+                                         end=max([span.end for span in nodes_in_comp[comp]]), box=merged_box))
+        return merged_spans
