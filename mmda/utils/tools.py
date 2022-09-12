1e85cb26aff639e6beabb4e23023695b28f91df0
Egor Klevak <egork@allenai.org>
2022-09-12 12:29:40 -0700

    Updating class so that the spans can be merged by Span index

--- mmda/utils/tools.py
+++ mmda/utils/tools.py
@@ -76,19 +76,45 @@ class MergeSpans:
     Given w=width and h=height overlap neighboring spans which are w, h or less apart
     Inspired by https://leetcode.com/problems/merge-intervals/
     """
-    def __init__(self, list_of_spans: List["Span"], w: float, h: float) -> None:
+    def __init__(self, list_of_spans: List["Span"], w: float = 0, h: float = 0, distance: int = 1) -> None:
+        """
+        Args
+            w (float): The input width between boxes to merge
+            h (float): The input height between the boxes to merge
+            distance (int): Distance between the spans
+        """
         self.list_of_spans = list_of_spans
         self.w = w
         self.h = h
         self.graph = defaultdict(list)
+        self.distance = distance
 
-    def build_graph(self):
+    def build_graph_index_overlap(self):
+        """
+        Build graph, each node is represented by (start, end) of tuple, with the list of spans with overlapping
+        boxes given, w, h
+        """
+        is_neighboring_spans = (
+            lambda span1, span2: min(
+                abs(span1.start - span2.end), abs(span1.end - span2.start)
+            ) <= self.distance
+        )
+
+        for i, span_i in enumerate(self.list_of_spans):
+            for j in range(i + 1, len(self.list_of_spans)):
+                if is_neighboring_spans(span_i, self.list_of_spans[j]):
+                    self.graph[span_i.start, span_i.end].append(self.list_of_spans[j])
+                    self.graph[self.list_of_spans[j].start, self.list_of_spans[j].end].append(span_i)
+
+    def build_graph_box_overlap(self):
         """
         Build graph, each node is represented by (start, end) of tuple, with the list of spans with overlapping
         boxes given, w, h
         """
         for i, span_i in enumerate(self.list_of_spans):
+            assert hasattr(span_i, 'box'), 'Missing attribute box in a span'
             for j in range(i + 1, len(self.list_of_spans)):
+                assert hasattr(self.list_of_spans[j], 'box'), 'Missing attribute box in a span'
                 if span_i.box.is_overlap(self.list_of_spans[j].box, self.w, self.h):
                     self.graph[span_i.start, span_i.end].append(self.list_of_spans[j])
                     self.graph[self.list_of_spans[j].start, self.list_of_spans[j].end].append(span_i)
@@ -121,11 +147,12 @@ class MergeSpans:
 
         return nodes_in_comp, comp_number
 
-    def merge(self):
+    def merge_neighbor_spans_by_box_coordinate(self):
         """
-        For each of the lists of the connected nodes, merge boxes and find, min, max of the index
+        For each of the lists of the connected nodes determined by distnace between the boxes,
+        merge boxes and find, min, max of the index
         """
-        self.build_graph()
+        self.build_graph_box_overlap()
         nodes_in_comp, number_of_comps = self.get_components()
 
         # all intervals in each connected component must be merged.
@@ -136,3 +163,21 @@ class MergeSpans:
                 merged_spans.append(Span(start=min([span.start for span in nodes_in_comp[comp]]),
                                          end=max([span.end for span in nodes_in_comp[comp]]), box=merged_box))
         return merged_spans
+
+    def merge_neighbor_spans_boxes_by_symbol_distance(self):
+        """
+        For each of the lists of the connected nodes determined by symbol distance,
+        merge boxes and find, min, max of the index
+        """
+        self.build_graph_index_overlap()
+        nodes_in_comp, number_of_comps = self.get_components()
+
+        # all intervals in each connected component must be merged.
+        merged_spans = []
+        for comp in range(number_of_comps):
+            if nodes_in_comp[comp]:
+                merged_box = Box.small_boxes_to_big_box([span.box for span in nodes_in_comp[comp]])
+                merged_spans.append(Span(start=min([span.start for span in nodes_in_comp[comp]]),
+                                         end=max([span.end for span in nodes_in_comp[comp]]), box=merged_box))
+        return merged_spans
+
