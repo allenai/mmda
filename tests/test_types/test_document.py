a4328e03a0bb8c079bc875cafc37be2dd40d6af6
Chris Wilhelm <chris.wilhelm@gmail.com>
2022-09-28 16:56:25 -0700

    Allows empty annotations. (#147)
    * Allows empty annotations.
    
    Previously we would skip making attrs
    on the document to represent these empty
    annotation groups. This resulted in attr
    not found errors in contexts where an expected
    field was absent and legitimately empty.

---
+++ tests/test_types/test_document.py
@@ -0,0 +1,11 @@
+import unittest
+
+from mmda.types.document import Document
+
+
+class TestDocument(unittest.TestCase):
+    def test__empty_annotations_work(self):
+        doc = Document("This is a test document!")
+        annotations = []
+        doc.annotate(my_cool_field=annotations)
+        self.assertEqual(doc.my_cool_field, [])
