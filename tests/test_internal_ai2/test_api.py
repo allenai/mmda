115ad08ac9980e6734b9c52b4e3555cd1b789186
Egor Klevak <egork@allenai.org>
2022-10-06 16:13:09 -0700

    Egork/quick fix span to mmda (#156)
    * Fixing Vila version to 0.5.0
    
    * Reverting changes. from_json(self.dict()) seems to not know how to handle a dictionary
    
    * Didn't meant to commit changes to the setup.py
    
    * Adding unit tests for Box, Span, SpanGroup, BoxGroup
    
    * api fix
    
    Co-authored-by: Luca Soldaini <luca@soldaini.net>

--- tests/test_internal_ai2/test_api.py
+++ tests/test_internal_ai2/test_api.py
@@ -4,6 +4,9 @@ from pydantic.error_wrappers import ValidationError
 
 import ai2_internal.api as mmda_api
 import mmda.types.annotation as mmda_ann
+from mmda.types import Metadata
+from mmda.types.box import Box as mmdaBox
+from mmda.types.span import Span as mmdaSpan
 
 
 class ClassificationAttributes(mmda_api.Attributes):
@@ -70,3 +73,54 @@ class TestApi(unittest.TestCase):
         sg_ann.uuid = sg_ann_2.uuid = 'manually-fix-to-avoid-randomness'
 
         self.assertEqual(sg_ann, sg_ann_2)
+
+    def test_box(self):
+        box = mmda_api.Box(left=0.1, top=0.1, width=0.1, height=0.1, page=0)
+        assert box.to_mmda() == mmdaBox(l=0.1, t=0.1, w=0.1, h=0.1, page=0)
+        assert mmda_api.Box.from_mmda(box.to_mmda()) == box
+
+    def test_span(self):
+        span = mmda_api.Span(start=0, end=1, box=mmda_api.Box(left=0.1, top=0.1, width=0.1, height=0.1, page=0))
+        assert span.to_mmda() == mmdaSpan(start=0, end=1, box=mmdaBox(l=0.1, t=0.1, w=0.1, h=0.1, page=0))
+
+    def test_box_group(self):
+        box_group = mmda_api.BoxGroup(
+            boxes=[
+                mmda_api.Box(left=0.1, top=0.1, width=0.1, height=0.1, page=0)
+            ],
+            id=0,
+            type='test',
+            # these attributes are going to be discarded because
+            # BoxGroup is using the default Attributes class
+            attributes={'one': 'Test string'}
+        )
+
+        self.assertEqual(
+            mmda_api.BoxGroup.from_mmda(box_group.to_mmda()),
+            box_group
+        )
+
+
+    def test_span_group(self):
+        box_group = mmda_api.BoxGroup(
+            boxes=[
+                mmda_api.Box(left=0.1, top=0.1, width=0.1, height=0.1, page=0)
+            ],
+            id=0,
+            type='test',
+            attributes={'one': 'Test string'}
+        )
+
+        span_group = mmda_api.SpanGroup(
+            spans=[],
+            box_group=box_group,
+            attributes={'one': 'Test string'},
+            id=0,
+            type='test',
+            text='this is a test'
+        )
+
+        self.assertEqual(
+            mmda_api.SpanGroup.from_mmda(span_group.to_mmda()),
+            span_group
+        )
