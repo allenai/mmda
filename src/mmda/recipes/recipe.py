04ee4de1081342bb7bc7a5a237a7808d5788a558
kyleclo <kyleclo@uw.edu>
2023-03-09 14:33:29 -0800

    abstractmethod

--- src/mmda/recipes/recipe.py
+++ src/mmda/recipes/recipe.py
@@ -7,10 +7,14 @@
 
 from mmda.types import *
 
+from abc import abstractmethod
+
 
 class Recipe:
+    @abstractmethod
     def from_path(self, pdfpath: str) -> Document:
         raise NotImplementedError
 
+    @abstractmethod
     def from_doc(self, doc: Document) -> Document:
         raise NotImplementedError
