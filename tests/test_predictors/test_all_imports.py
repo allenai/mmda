1147f9252dc3ca241737fa6ea1f4aa8a94848b3e
kyleclo <kyleclo@uw.edu>
2023-03-13 10:17:35 -0700

    add test import

---
+++ tests/test_predictors/test_all_imports.py
@@ -0,0 +1,11 @@
+"""
+
+
+
+"""
+
+from mmda.predictors.hf_predictors import *
+from mmda.predictors.lp_predictors import *
+from mmda.predictors.xgb_predictors import *
+from mmda.predictors.heuristic_predictors import *
+from mmda.predictors.base_predictors import *
