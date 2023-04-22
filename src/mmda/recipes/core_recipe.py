2f3667f9f2ad7814579261ae664228a7a3075d6c
kyleclo <kyleclo@uw.edu>
2023-04-21 23:56:00 -0700

    formatting

--- src/mmda/recipes/core_recipe.py
+++ src/mmda/recipes/core_recipe.py
@@ -9,31 +9,39 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+from mmda.parsers.pdfplumber_parser import PDFPlumberParser
+from mmda.predictors.heuristic_predictors.dictionary_word_predictor import (
+    DictionaryWordPredictor,
+)
+from mmda.predictors.hf_predictors.token_classification_predictor import (
+    IVILATokenClassificationPredictor,
+)
+from mmda.predictors.lp_predictors import LayoutParserPredictor
+from mmda.rasterizers.rasterizer import PDF2ImageRasterizer
+from mmda.recipes.recipe import Recipe
 from mmda.types import *
 
 
-from mmda.parsers.pdfplumber_parser import PDFPlumberParser
-from mmda.rasterizers.rasterizer import PDF2ImageRasterizer
-from mmda.predictors.heuristic_predictors.dictionary_word_predictor import DictionaryWordPredictor
-from mmda.predictors.lp_predictors import LayoutParserPredictor
-from mmda.predictors.hf_predictors.token_classification_predictor import \
-    IVILATokenClassificationPredictor
-from mmda.recipes.recipe import Recipe
-
-
 class CoreRecipe(Recipe):
-    def __init__(self,
-                 effdet_publaynet_predictor_path: str = 'lp://efficientdet/PubLayNet',
-                 effdet_mfd_predictor_path: str = 'lp://efficientdet/MFD',
-                 vila_predictor_path: str = 'allenai/ivila-row-layoutlm-finetuned-s2vl-v2'):
+    def __init__(
+        self,
+        effdet_publaynet_predictor_path: str = "lp://efficientdet/PubLayNet",
+        effdet_mfd_predictor_path: str = "lp://efficientdet/MFD",
+        vila_predictor_path: str = "allenai/ivila-row-layoutlm-finetuned-s2vl-v2",
+    ):
         logger.info("Instantiating recipe...")
         self.parser = PDFPlumberParser()
         self.rasterizer = PDF2ImageRasterizer()
         self.word_predictor = DictionaryWordPredictor()
         self.effdet_publaynet_predictor = LayoutParserPredictor.from_pretrained(
-            effdet_publaynet_predictor_path)
-        self.effdet_mfd_predictor = LayoutParserPredictor.from_pretrained(effdet_mfd_predictor_path)
-        self.vila_predictor = IVILATokenClassificationPredictor.from_pretrained(vila_predictor_path)
+            effdet_publaynet_predictor_path
+        )
+        self.effdet_mfd_predictor = LayoutParserPredictor.from_pretrained(
+            effdet_mfd_predictor_path
+        )
+        self.vila_predictor = IVILATokenClassificationPredictor.from_pretrained(
+            vila_predictor_path
+        )
         logger.info("Finished instantiating recipe")
 
     def from_path(self, pdfpath: str) -> Document:
