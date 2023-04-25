e1cbbf77fc7c7a4a5858d9bcbbfb701f5bbb98b6
kyleclo <kyleclo@uw.edu>
2023-04-24 19:37:07 -0700

    add boxgroup indexer; and tests

--- tests/test_types/test_indexers.py
+++ tests/test_types/test_indexers.py
@@ -1,7 +1,7 @@
 import unittest
 
-from mmda.types import Span, SpanGroup
-from mmda.types.indexers import SpanGroupIndexer
+from mmda.types import Box, BoxGroup, Span, SpanGroup
+from mmda.types.indexers import BoxGroupIndexer, SpanGroupIndexer
 
 
 class TestSpanGroupIndexer(unittest.TestCase):
@@ -37,3 +37,44 @@ class TestSpanGroupIndexer(unittest.TestCase):
 
         self.assertEqual(len(matches), 2)
         self.assertEqual(matches, [span_groups_to_index[0], span_groups_to_index[1]])
+
+
+class TestBoxGroupIndexer(unittest.TestCase):
+    def test_overlap_within_single_boxgroup_fails_checks(self):
+        box_groups = [
+            BoxGroup(
+                id=1,
+                boxes=[Box(0, 0, 5, 5, page=0), Box(4, 4, 7, 7, page=0)],
+                allow_overlap=True,
+            )
+        ]
+
+        with self.assertRaises(ValueError):
+            BoxGroupIndexer(box_groups)
+
+    def test_overlap_between_boxgroups_fails_checks(self):
+        box_groups = [
+            BoxGroup(
+                id=1, boxes=[Box(0, 0, 5, 5, page=0), Box(5.01, 5.01, 8, 8, page=0)]
+            ),
+            BoxGroup(id=2, boxes=[Box(6, 6, 10, 10, page=0)]),
+        ]
+
+        with self.assertRaises(ValueError):
+            BoxGroupIndexer(box_groups)
+
+    def test_finds_matching_groups_in_doc_order(self):
+        box_groups_to_index = [
+            BoxGroup(id=1, boxes=[Box(0, 0, 1, 1, page=0), Box(2, 2, 1, 1, page=0)]),
+            BoxGroup(id=2, boxes=[Box(4, 4, 1, 1, page=0)]),
+            BoxGroup(id=3, boxes=[Box(100, 100, 1, 1, page=0)]),
+        ]
+
+        index = BoxGroupIndexer(box_groups_to_index)
+
+        # should intersect 1 and 2 but not 3
+        probe = BoxGroup(id=4, boxes=[Box(1, 1, 5, 5, page=0), Box(9, 9, 5, 5, page=0)])
+        matches = index.find(probe)
+
+        self.assertEqual(len(matches), 2)
+        self.assertEqual(matches, [box_groups_to_index[0], box_groups_to_index[1]])
