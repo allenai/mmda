23709e06f1e2d3ee2c993d649fdebd300779faf6
Angele Zamarron <angelez@allenai.org>
2023-05-12 11:26:34 -0700

    refactored still gets bibs

--- src/mmda/parsers/grobid_augment_existing_document_parser.py
+++ src/mmda/parsers/grobid_augment_existing_document_parser.py
@@ -3,15 +3,16 @@
 @geli-gel
 
 """
+from collections import defaultdict
 from grobid_client.grobid_client import GrobidClient
-from typing import Optional, List
+from typing import Dict, Iterable, List, Optional
 
 import os
 import xml.etree.ElementTree as et
 
 from mmda.parsers.parser import Parser
 from mmda.types import Metadata
-from mmda.types.annotation import BoxGroup, Box
+from mmda.types.annotation import BoxGroup, Box, SpanGroup
 from mmda.types.document import Document
 from mmda.types.names import PagesField, RowsField, TokensField
 from mmda.utils.tools import box_groups_to_span_groups
@@ -34,24 +35,23 @@ class GrobidAugmentExistingDocumentParser(Parser):
 
         self.client = GrobidClient(config_path=config_path, check_server=check_server)
 
-
     def parse(self, input_pdf_path: str, doc: Document, xml_out_dir: Optional[str] = None) -> Document:
-        
+
         assert doc.symbols != ""
         for field in REQUIRED_DOCUMENT_FIELDS:
             assert field in doc.fields
 
         (_, _, xml) = self.client.process_pdf(
-                "processFulltextDocument",
-                input_pdf_path,
-                generateIDs=False,
-                consolidate_header=False,
-                consolidate_citations=False,
-                include_raw_citations=False,
-                include_raw_affiliations=False,
-                tei_coordinates=True,
-                segment_sentences=True
-            )
+            "processFulltextDocument",
+            input_pdf_path,
+            generateIDs=False,
+            consolidate_header=False,
+            consolidate_citations=False,
+            include_raw_citations=False,
+            include_raw_affiliations=False,
+            tei_coordinates=True,
+            segment_sentences=True
+        )
         if xml_out_dir:
             os.makedirs(xml_out_dir, exist_ok=True)
             xmlfile = os.path.join(xml_out_dir, os.path.basename(input_pdf_path).replace('.pdf', '.xml'))
@@ -61,19 +61,25 @@ class GrobidAugmentExistingDocumentParser(Parser):
         self._parse_xml_onto_doc(xml, doc)
         return doc
 
+    def _grobid_box_groups_to_span_groups(self, doc: Document, **kwargs: Iterable[BoxGroup]) -> Dict[str, List[SpanGroup] ]:
+        span_groups_by_field_name = defaultdict(list)
+        for parsed_field_name, box_groups in kwargs.items():
+            span_groups_by_field_name[parsed_field_name] = box_groups_to_span_groups(box_groups, doc, center=True)
+        return span_groups_by_field_name
+
     def _parse_xml_onto_doc(self, xml: str, doc: Document) -> Document:
         xml_root = et.fromstring(xml)
+        box_group_parses = defaultdict(list)
 
-        bib_entry_box_groups = self._get_grobid_bib_box_groups(xml_root)
-
-        # get nice SpanGroups from tokens whose centers overlap with the Grobid box
-        bib_entry_span_groups = box_groups_to_span_groups(bib_entry_box_groups, doc, center=True)
-
+        # get raw box groups from Grobid XML output
+        # box_group_parses["authors"] = self._get_author_box_groups(xml_root) # TODO turn on/make
         # note for if/when adding in relations between mention sources and bib targets:
         # big_entries metadata contains original grobid id attached to the BoxGroup.
-        doc.annotate(
-            bib_entries=bib_entry_span_groups
-            )
+        box_group_parses["bib_entries"] = self._get_bib_entry_box_groups(xml_root)
+
+        # get nice SpanGroups from tokens whose centers overlap with the Grobid boxes
+        annotatable_span_groups_by_field_name = self._grobid_box_groups_to_span_groups(doc, **box_group_parses)
+        doc.annotate(**annotatable_span_groups_by_field_name)
 
         return doc
 
@@ -85,16 +91,16 @@ class GrobidAugmentExistingDocumentParser(Parser):
             proper_page = int(pg) - 1
             boxes.append(
                 Box(
-                    l=float(x), 
-                    t=float(y), 
-                    w=float(w), 
-                    h=float(h), 
+                    l=float(x),
+                    t=float(y),
+                    w=float(w),
+                    h=float(h),
                     page=proper_page
                 ).get_relative(*page_sizes[proper_page])
             )
         return boxes
 
-    def _get_grobid_bib_box_groups(self, root: et.Element) -> List[BoxGroup]:
+    def _get_bib_entry_box_groups(self, root: et.Element) -> List[BoxGroup]:
         bib_list_root = root.find(".//tei:listBibl", NS)
 
         page_size_root = root.find(".//tei:facsimile", NS)
@@ -102,7 +108,7 @@ class GrobidAugmentExistingDocumentParser(Parser):
         page_sizes = dict()
         for data in page_size_data:
             page_sizes[int(data.attrib["n"]) - 1] = [float(data.attrib["lrx"]), float(data.attrib["lry"])]
-        
+
         grobid_bibs = []
         bib_structs = bib_list_root.findall(".//tei:biblStruct", NS)
         for bib in bib_structs:
@@ -112,10 +118,9 @@ class GrobidAugmentExistingDocumentParser(Parser):
 
             grobid_bibs.append(
                 BoxGroup(
-                boxes=boxes, 
-                metadata=Metadata(grobid_id=grobid_id)
+                    boxes=boxes,
+                    metadata=Metadata(grobid_id=grobid_id)
                 )
             )
 
         return grobid_bibs
-    
