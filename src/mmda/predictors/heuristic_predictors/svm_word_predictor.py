667744b7ca4d2db6cb1626ca191a02024ee8d523
kyleclo <kyleclo@uw.edu>
2023-05-17 10:20:33 -0700

    model and tests

---
+++ src/mmda/predictors/heuristic_predictors/svm_word_predictor.py
@@ -0,0 +1,251 @@
+"""
+
+
+"""
+
+import re
+from collections import defaultdict
+from typing import Dict, List
+
+import numpy as np
+from joblib import load
+from scipy.sparse import hstack
+
+from mmda.predictors.base_predictors.base_predictor import BasePredictor
+from mmda.predictors.heuristic_predictors.whitespace_predictor import (
+    WhitespacePredictor,
+)
+from mmda.types import Document, Metadata, SpanGroup
+
+
+class SVMWordPredictor(BasePredictor):
+    def __init__(self, ohe_encoder, scaler, estimator, unigram_probs):
+        self.ohe_encoder = ohe_encoder
+        self.scaler = scaler
+        self.estimator = estimator
+        self.unigram_probs = unigram_probs
+        self.default_prob = unigram_probs["<unk>"]
+        self.sparse_columns = [
+            "shape",
+            "s_bg1",
+            "s_bg2",
+            "s_bg3",
+            "p_bg1",
+            "p_bg2",
+            "p_bg3",
+            "p_lower",
+            "s_lower",
+        ]
+        self.dense_columns = [
+            "p_upper",
+            "s_upper",
+            "p_number",
+            "s_number",
+            "p_isalpha",
+            "s_isalpha",
+            "p_len",
+            "s_len",
+            "multi_hyphen",
+            "uni_prob",
+        ]
+        self.whitespace_predictor = WhitespacePredictor()
+
+    @classmethod
+    def from_path(
+        cls,
+        ohe_encoder_path: str,
+        scaler_path: str,
+        estimator_path: str,
+        unigram_probs_path: str,
+    ):
+        ohe_encoder = load(ohe_encoder_path)
+        scaler = load(scaler_path)
+        estimator = load(estimator_path)
+        unigram_probs = load(unigram_probs_path)
+        predictor = SVMWordPredictor(
+            ohe_encoder=ohe_encoder,
+            scaler=scaler,
+            estimator=estimator,
+            unigram_probs=unigram_probs,
+        )
+        return predictor
+
+    def predict(self, document: Document) -> List[SpanGroup]:
+        # validate input
+        self._validate_tokenization(document=document)
+
+        # precompute whitespace tokenization
+        clusters = self._cluster_tokens_by_whitespace(document=document)
+
+        # initialize output format
+        token_id_to_word_id = {t.id: None for t in document.tokens}
+        word_id_to_text = {}
+
+        for token_ids in clusters:
+            tokens = [document.tokens[token_id] for token_id in token_ids]
+
+        # convert format
+        words = self._create_words(
+            document=document,
+            token_id_to_word_id=token_id_to_word_id,
+            word_id_to_text=word_id_to_text,
+        )
+        return words
+
+    def _get_dense_features(self, part: str, name_prefix: str):
+        upper = int(part[0].isupper())
+        number = int(part.isnumeric())
+        alpha = int(part.isalpha())
+        lower = part.lower()
+        plen = len(part)
+        return {
+            f"{name_prefix}_upper": upper,
+            f"{name_prefix}_number": number,
+            f"{name_prefix}_isalpha": alpha,
+            f"{name_prefix}_len": plen,
+        }
+
+    def _get_features(self, words: List[str]):
+        sparse_all, dense_all = [], []
+        idx, word_id_to_feature_ids = 0, dict()
+        for widx, word in enumerate(words):
+            split = word.split("-")
+            for i, s in enumerate(split[:-1]):
+                sparse_features, dense_features = dict(), dict()
+                prefix = "-".join(split[: i + 1])
+                suffix = "-".join(split[i + 1 :])
+                if widx not in word_id_to_feature_ids:
+                    word_id_to_feature_ids[widx] = []
+                word_id_to_feature_ids[widx].append(idx)
+                idx += 1
+                dense_features.update(self._get_dense_features(prefix, "p"))
+                dense_features.update(self._get_dense_features(suffix, "s"))
+                orig_uni_prob = self.unigram_probs.get(word, self.default_prob)
+                presuf_uni_prob = self.unigram_probs.get(
+                    f"{prefix}{suffix}", self.default_prob
+                )
+                dense_features["uni_prob"] = orig_uni_prob - presuf_uni_prob
+                dense_features["multi_hyphen"] = int(word.count("-") > 1)
+                sparse_features["shape"] = re.sub("\w", "x", word)
+                sparse_features["s_lower"] = suffix.lower()
+                sparse_features["s_bg1"] = suffix[:2]
+                sparse_features["s_bg2"] = suffix[1:3] if len(suffix) > 2 else ""
+                sparse_features["s_bg3"] = suffix[2:4] if len(suffix) > 3 else ""
+                sparse_features["p_lower"] = prefix.lower()
+                sparse_features["p_bg1"] = prefix[-2:][::-1] if len(prefix) > 1 else ""
+                sparse_features["p_bg2"] = (
+                    prefix[-3:-1][::-1] if len(prefix) > 2 else ""
+                )
+                sparse_features["p_bg3"] = (
+                    prefix[-4:-2][::-1] if len(prefix) > 3 else ""
+                )
+                sparse_all.append([sparse_features[k] for k in self.sparse_columns])
+                dense_all.append([dense_features[k] for k in self.dense_columns])
+        dense_transformed = self.scaler.transform(dense_all)
+        sparse_transformed = self.ohe_encoder.transform(sparse_all)
+
+        return hstack([sparse_transformed, dense_transformed]), word_id_to_feature_ids
+
+    def _predict(self, words: List[str], threshold: float) -> List[Dict]:
+        all_features, word_id_to_feature_ids = self._get_features(words)
+        all_scores = self.estimator.decision_function(all_features)
+        results = []
+        for word_id, feature_ids in word_id_to_feature_ids.items():
+            word = words[word_id]
+            word_segments = word.split("-")
+            score_per_hyphen_in_word = all_scores[feature_ids]
+            new_word = word_segments[0]
+            for word_segment, hyphen_score in zip(
+                word_segments[1:], score_per_hyphen_in_word
+            ):
+                if hyphen_score > threshold:
+                    new_word += "-"
+                else:
+                    new_word += ""
+                new_word += word_segment
+            results.append(
+                {"original": word, "new": new_word, "is_edit": word != new_word}
+            )
+        return results
+
+    def _validate_tokenization(self, document: Document):
+        """This Word Predictor relies on a specific type of Tokenization
+        in which hyphens ('-') must be their own token. This verifies."""
+        for token in document.tokens:
+            if "-" in token.text and token.text != "-":
+                raise ValueError(
+                    f"Document contains Token with hyphen, but not as its own token."
+                )
+
+    def _cluster_tokens_by_whitespace(self, document: Document) -> List[List[int]]:
+        """
+        `whitespace_tokenization` is necessary because lack of whitespace is an indicator that
+        adjacent tokens belong in a word together.
+        """
+        _ws_tokens: List[SpanGroup] = self.whitespace_predictor.predict(
+            document=document
+        )
+        document.annotate(_ws_tokens=_ws_tokens)
+
+        # token -> ws_tokens
+        token_id_to_ws_token_id = {}
+        for token in document.tokens:
+            token_id_to_ws_token_id[token.id] = token._ws_tokens[0].id
+
+        # ws_token -> tokens
+        ws_token_id_to_tokens = defaultdict(list)
+        for token_id, ws_token_id in token_id_to_ws_token_id.items():
+            ws_token_id_to_tokens[ws_token_id].append(token_id)
+
+        # token -> all cluster tokens (inclusive)
+        # token_id_to_token_ids = {}
+        # for token_id, ws_token_id in token_id_to_ws_token_id.items():
+        #     candidate_token_ids = [i for i in ws_token_id_to_tokens[ws_token_id]]
+        #     token_id_to_token_ids[token_id] = candidate_token_ids
+
+        # cluster tokens by whitespace
+        clusters = [
+            sorted(ws_token_id_to_tokens[ws_token_id])
+            for ws_token_id in range(len(ws_token_id_to_tokens))
+        ]
+        return clusters
+
+    def _get_word_candidates(
+        self, document: Document, token_clusters: List[List[int]]
+    ) -> List[List[int]]:
+        # TODO: stopped here
+        candidates = []
+        for token_cluster in token_clusters:
+            tokens = [document.tokens[token_id] for token_id in token_cluster]
+            if tokens[-1].text == "-":
+                pass
+        return candidates
+
+    def _create_words(
+        self, document: Document, token_id_to_word_id, word_id_to_text
+    ) -> List[SpanGroup]:
+        words = []
+        tokens_in_word = [document.tokens[0]]
+        current_word_id = 0
+        for token_id in range(1, len(token_id_to_word_id)):
+            token = document.tokens[token_id]
+            word_id = token_id_to_word_id[token_id]
+            if word_id == current_word_id:
+                tokens_in_word.append(token)
+            else:
+                word = SpanGroup(
+                    spans=[span for token in tokens_in_word for span in token.spans],
+                    id=current_word_id,
+                    metadata=Metadata(text=word_id_to_text[current_word_id]),
+                )
+                words.append(word)
+                tokens_in_word = [token]
+                current_word_id = word_id
+        # last bit
+        word = SpanGroup(
+            spans=[span for token in tokens_in_word for span in token.spans],
+            id=current_word_id,
+            metadata=Metadata(text=word_id_to_text[current_word_id]),
+        )
+        words.append(word)
+        return words
---
+++ tests/test_predictors/test_svm_word_predictor.py
@@ -0,0 +1,167 @@
+"""
+Tests for SVM Word Predictor
+
+@kylel
+"""
+
+import json
+import os
+import unittest
+from typing import List, Optional, Set
+
+import numpy as np
+
+from mmda.predictors.heuristic_predictors.svm_word_predictor import SVMWordPredictor
+from mmda.types import Document, Span, SpanGroup
+
+
+class TestSVMWordPredictor(unittest.TestCase):
+    @classmethod
+    def setUp(self):
+        self.fixture_path = os.path.join(os.path.dirname(__file__), "../fixtures/")
+        self.predictor = SVMWordPredictor.from_path(
+            ohe_encoder_path=os.path.join(
+                self.fixture_path, "svm_word_predictor/ohencoder.joblib"
+            ),
+            scaler_path=os.path.join(
+                self.fixture_path, "svm_word_predictor/scaler.joblib"
+            ),
+            estimator_path=os.path.join(
+                self.fixture_path, "svm_word_predictor/hyphen_clf.joblib"
+            ),
+            unigram_probs_path=os.path.join(
+                self.fixture_path, "svm_word_predictor/unigram_probs.pkl"
+            ),
+        )
+        with open(
+            os.path.join(self.fixture_path, "svm_word_predictor/pos_words.txt")
+        ) as f_in:
+            self.pos_words = [line.strip() for line in f_in]
+        with open(
+            os.path.join(self.fixture_path, "svm_word_predictor/neg_words.txt")
+        ) as f_in:
+            self.neg_words = [line.strip() for line in f_in]
+
+        with open(
+            os.path.join(
+                self.fixture_path,
+                "types/20fdafb68d0e69d193527a9a1cbe64e7e69a3798__pdfplumber_doc.json",
+            )
+        ) as f_in:
+            doc_dict = json.load(f_in)
+            self.doc = Document.from_json(doc_dict=doc_dict)
+
+    def test_get_features(self):
+        all_features, word_id_to_feature_ids = self.predictor._get_features(
+            words=self.pos_words
+        )
+        self.assertEqual(len(word_id_to_feature_ids), len(self.pos_words))
+        self.assertEqual(
+            all_features.shape[0],
+            sum([len(feature) for feature in word_id_to_feature_ids.values()]),
+        )
+        all_features, word_id_to_feature_ids = self.predictor._get_features(
+            words=self.neg_words
+        )
+        self.assertEqual(len(word_id_to_feature_ids), len(self.neg_words))
+        self.assertEqual(
+            all_features.shape[0],
+            sum([len(feature) for feature in word_id_to_feature_ids.values()]),
+        )
+
+    def test__predict(self):
+        pos_words = [
+            "wizard-of-oz",
+            "moment-to-moment",
+            "batch-to-batch",
+            "Seven-day-old",
+            "slow-to-fast",
+            "HTLV-1-associated",
+            "anti-E-selectin",
+        ]
+        neg_words = [
+            "sig-nal-to-noise",
+            "nonre-turn-to-zero",
+            "comput-er-assisted",
+            "concentra-tion-dependent",
+            "ob-ject-oriented",
+            "cog-nitive-behavioral",
+            "deci-sion-makers",
+        ]
+        THRESHOLD = -1.0
+        pos_results = self.predictor._predict(words=pos_words, threshold=THRESHOLD)
+        self.assertEqual(len(pos_results), len(pos_words))
+        self.assertTrue(all([r["is_edit"] is False for r in pos_results]))
+        neg_results = self.predictor._predict(words=neg_words, threshold=THRESHOLD)
+        self.assertEqual(len(neg_results), len(neg_words))
+        self.assertTrue(all([r["is_edit"] is True for r in neg_results]))
+
+    def test_validate_tokenization(self):
+        doc = Document.from_json(
+            doc_dict={
+                "symbols": "I am the wizard-of-oz.",
+                "tokens": [
+                    {"spans": [{"start": 0, "end": 1}]},
+                    {"spans": [{"start": 2, "end": 4}]},
+                    {"spans": [{"start": 5, "end": 8}]},
+                    {"spans": [{"start": 9, "end": 15}]},
+                    {"spans": [{"start": 15, "end": 16}]},
+                    {"spans": [{"start": 16, "end": 18}]},
+                    {"spans": [{"start": 18, "end": 19}]},
+                    {"spans": [{"start": 19, "end": 21}]},
+                    {"spans": [{"start": 21, "end": 22}]},
+                ],
+            }
+        )
+        self.predictor._validate_tokenization(document=doc)
+        with self.assertRaises(ValueError):
+            doc = Document.from_json(
+                doc_dict={
+                    "symbols": "I am the wizard-of-oz.",
+                    "tokens": [
+                        {"spans": [{"start": 0, "end": 1}]},
+                        {"spans": [{"start": 2, "end": 4}]},
+                        {"spans": [{"start": 5, "end": 8}]},
+                        {"spans": [{"start": 9, "end": 18}]},
+                        {"spans": [{"start": 18, "end": 19}]},
+                        {"spans": [{"start": 19, "end": 21}]},
+                        {"spans": [{"start": 21, "end": 22}]},
+                        {"spans": [{"start": 22, "end": 23}]},
+                    ],
+                }
+            )
+            self.predictor._validate_tokenization(document=doc)
+
+    def test_cluster_tokens_by_whitespace(self):
+        clusters = self.predictor._cluster_tokens_by_whitespace(document=self.doc)
+        for token_ids in clusters:
+            if len(token_ids) == 1:
+                token = self.doc.tokens[token_ids[0]]
+                self.assertEqual(
+                    self.doc.symbols[token.spans[0].start : token.spans[0].end],
+                    token.text,
+                )
+            else:
+                tokens = [self.doc.tokens[token_id] for token_id in token_ids]
+                spans = [span for token in tokens for span in token.spans]
+                big_span = Span.small_spans_to_big_span(spans=spans)
+                self.assertEqual(
+                    self.doc.symbols[big_span.start : big_span.end],
+                    "".join([token.text for token in tokens]),
+                )
+
+    def test_predict(self):
+        preds_pos = self.predictor._predict(words=self.pos_words)
+        self.assertEqual(len(preds_pos), len(self.pos_words))
+        tp = sum(preds_pos)
+        fn = len(preds_pos) - tp
+
+        preds_neg = self.predictor._predict(words=self.neg_words)
+        self.assertEqual(len(preds_neg), len(self.neg_words))
+        tn = sum(preds_neg)
+        fp = len(preds_neg) - tn
+
+        self.assertEqual(tp + fn + tn + fp, len(preds_pos) + len(preds_neg))
+
+        p = tp / (tp + fp)
+        r = tp / (tp + fn)
