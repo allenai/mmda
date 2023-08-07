09f42f625192f744efa433d13ddbe050cc838b3b
Dany Haddad <danyhaddad43@gmail.com>
2023-08-07 12:18:09 -0500

    merge boxes from citation mention model

--- src/ai2_internal/citation_mentions/interface.py
+++ src/ai2_internal/citation_mentions/interface.py
@@ -39,6 +39,34 @@ class PredictorConfig(BaseSettings):
     pass
 
 
+def group_by_line(boxes):
+    boxes = sorted(boxes, key=lambda box: box.t)
+    return [list(line_boxes) for t, line_boxes in groupby(boxes, key=lambda box: box.t)]
+
+def calc_bounding_box(boxes):
+    l = min(b.l for b in boxes)
+    t = min(b.t for b in boxes)
+    w = max(b.l + b.w for b in boxes) - l
+    h = max(b.t + b.h for b in boxes) - t
+    return mmda.types.box.Box(l=l, t=t, w=w, h=h, page=boxes[0].page)
+
+def merge_boxes(boxes):
+    boxes_by_line = group_by_line(boxes)
+    return [calc_bounding_box(line_boxes) for line_boxes in boxes_by_line]
+
+def merge_boxes_of_sg(sg):
+    sg.box_group.boxes = merge_boxes(sg.box_group.boxes)
+
+def all_spans_close(sg):
+    spans = sorted(sg.spans, key=lambda span: span.start)
+    return all(span.end <= next_span.start <= span.end + 5 for span, next_span in zip(spans, spans[1:]))
+
+def merge_citation_mention_boxes(citation_mentions):
+    for sg in citation_mentions:
+        if all_spans_close(sg):
+            merge_boxes_of_sg(sg)
+
+
 class Predictor:
     """
     Interface on to your underlying model.
@@ -64,7 +92,9 @@ class Predictor:
         doc.annotate(pages=[sg.to_mmda() for sg in inst.pages])
 
         prediction_span_groups = self._predictor.predict(doc)
+        merge_citation_mention_boxes(prediction_span_groups)
         doc.annotate(citation_mentions=prediction_span_groups)
+
 
         return Prediction(mentions=[api.SpanGroup.from_mmda(sg) for sg in doc.citation_mentions])
 
